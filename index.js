fce56980244a6b28c7f50a96a569b5a3ea9847bb
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro.local>
2022-09-09 09:42:23 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const date = new Date();
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+];
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send(`id was not found`);
+    res.send(course)
+});
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Lisening on port ${port} ${date}...`)
+});
