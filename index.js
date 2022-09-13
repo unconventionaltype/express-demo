966605f3cfb7c92e9575376ce9b282b3574ccc6e
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro.local>
2022-09-13 11:10:24 -0600

    last express

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
+
 const app = express();
+app.use(express.json());
 const date = new Date();
 
 const courses = [
@@ -20,9 +23,54 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send(`id ${req.params.id} was not found`);
+    if (!course) return res.status(404).send(`id ${req.params.id} was not found`);
     res.send(course)
 });
+
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body)
+    if (error) return res.status(400).send(result.error.details[0].message);
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+
+})
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send(`id ${req.params.id} was not found`);
+
+    const { error } = validateCourse(req.body)
+    if (error) return res.status(400).send(result.error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+
+})
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send(`id ${req.params.id} was not found`);
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+})
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+
+}
 
 const port = process.env.PORT || 3000
 
